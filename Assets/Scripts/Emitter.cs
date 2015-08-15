33e05a0359cf47f0bea293a207b17ded1f20020d
c_plus_c <dtm.music.producer@gmail.com>
2015-08-15 23:22:54 +0900

    finished #10

--- Assets/Scripts/Emitter.cs
+++ Assets/Scripts/Emitter.cs
@@ -7,6 +7,8 @@ public class Emitter : MonoBehaviour {
 
 	private int currentWave;
 
+	private Manager manager;
+
 	// Use this for initialization
 	IEnumerator Start () {
 
@@ -14,16 +16,24 @@ public class Emitter : MonoBehaviour {
 			yield break;
 		}
 
-		while (true) {
-			GameObject wave = (GameObject)Instantiate(waves[currentWave],transform.position,Quaternion.identity);
-			wave.transform.parent = transform;
+		manager = FindObjectOfType<Manager> ();
 
-			while(wave.transform.childCount != 0)
+		while (true) {
+
+			while(manager.IsPlaying() == false)
 			{
 				yield return new WaitForEndOfFrame();
 			}
 
-			Destroy(wave);
+			GameObject g = (GameObject)Instantiate(waves[currentWave],transform.position,Quaternion.identity);
+			g.transform.parent = transform;
+
+			while(g.transform.childCount != 0)
+			{
+				yield return new WaitForEndOfFrame();
+			}
+
+			Destroy(g);
 
 			if(waves.Length <= ++currentWave)
 			{
